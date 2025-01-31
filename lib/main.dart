2835b01bdbe8156f1eb4a285be8fbf769ee0de4b
thisismostafahassan <iammostafahassan@gmail.com>
2025-01-31 19:59:19 +0200

    first submit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/observer/my_bloc_observer.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+}
