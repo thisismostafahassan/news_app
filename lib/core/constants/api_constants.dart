5737415e75bf34e64d16fd9d2941c4b766ed2007
thisismostafahassan <iammostafahassan@gmail.com>
2025-02-02 20:39:45 +0200

    api constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,10 +1,18 @@
 class ApiConstants {
   // API url
-  static const String baseUrl = "https://newsapi.org/";
+  static const String baseUrl = "https://newsapi.org/v2";
 
   // api key
   static const String apiKey = '073429271a3e47f8a17ad71f0143370e';
-  // Technology
+  // everything
   static const String everything = '$baseUrl/everything?q=all&apikey=$apiKey';
-  //
+  // Top-Headlines
+  static const String topHeadlines =
+      '$baseUrl/top-headlines?country=us&apiKey=$apiKey';
+  // All sources
+  static const String allSources =
+      '$baseUrl/top-headlines/sources?country=us&apiKey=$apiKey';
+  // Sports
+  static const String sports =
+      '$baseUrl/top-headlines?category=sports&apiKey=$apiKey';
 }
